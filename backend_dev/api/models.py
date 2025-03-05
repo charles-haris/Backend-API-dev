6476a4d583aa10d8c541e9e179d18b4ae2031aa9
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-03-05 16:31:09 +0000

    models added

--- backend_dev/api/models.py
+++ backend_dev/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Permission(models.Model):
+    permission_name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.permission_name
+
+class Role(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    permissions = models.ManyToManyField(Permission, related_name='roles')
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # Use Django's built-in password hashing
+    is_active = models.BooleanField(default=True)
+    roles = models.ManyToManyField(Role, related_name='users')
+
+    def __str__(self):
+        return self.email
